deaa2bd1286121f0f4bf385e53a8dcfdd1ddc8c3
Jenilee_Bahan <bahanjennie@gmail.com>
2024-12-07 06:00:07 +0800

    Update ChatbotToggle.js

--- src/components/Chatbot/ChatbotToggle.js
+++ src/components/Chatbot/ChatbotToggle.js
@@ -11,7 +11,7 @@ const ChatbotToggle = ({chatbotHidden, setChatbotHidden, firstOpen, setFirstOpen
   }
   return (
     <div className={`${style.chatbotToggle} ${style.slideLeftIn}`}>
-      <button id="openChat" className={`text-white py-2 px-4 rounded-md transition duration-300 flex items-center ${style.chatbotIcon} ${style.toggleButton}`} onClick={toggleChatbot}>
+      <button id="openChat" className={`${style.chatBox} ${style.chatbotIcon} ${style.toggleButton}`} onClick={toggleChatbot}>
         {/* <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
         </svg> */}
